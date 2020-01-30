11b9e031fa921bf5bc6ef7d07ccaa9b7ac362aef
Dmitry <bdv.s4kt@gmail.com>
2020-01-30 12:31:46 -0500

    Update sayhello.go

--- handler/sayhello.go
+++ handler/sayhello.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// SayHello is a simple handler which print message on server side and return simple HTML as response
+// SayHello is a simple handler which print message on server side and return simple HTML as a response
 func SayHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("This is server side message")
 	fmt.Fprintln(w, `<b>Response</b> from <i>server!</i>`)
